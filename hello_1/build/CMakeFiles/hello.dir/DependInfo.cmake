e2be6f36d3db296840e7161da64dc9d92fdb9717
李明星 <limingxing03@meituan.com>
2019-03-29 20:00:34 +0800

    x

--- hello_1/build/CMakeFiles/hello.dir/DependInfo.cmake
+++ hello_1/build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/lmxing/Documents/code/cpp/cmake/hello/main.c" "/Users/lmxing/Documents/code/cpp/cmake/hello/build/CMakeFiles/hello.dir/main.c.o"
+  "/Users/lmxing/Documents/code/cpp/cmake/hello_1/main.c" "/Users/lmxing/Documents/code/cpp/cmake/hello_1/build/CMakeFiles/hello.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "Clang")
 
--- hello_1/build/cmake_install.cmake
+++ hello_1/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/lmxing/Documents/code/cpp/cmake/hello
+# Install script for directory: /Users/lmxing/Documents/code/cpp/cmake/hello_1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/lmxing/Documents/code/cpp/cmake/hello/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/lmxing/Documents/code/cpp/cmake/hello_1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
